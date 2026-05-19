bb6b2ce101b854e862b83a2844e5d32e340d6894
anykey196 <anykey196@gmqil.com>
2026-05-19 16:01:24 +0500

    Add back @AndroidEntryPoint + AppUpdateRepository injection, keep plain text

--- app/src/main/java/com/qrcode/scanner/MainActivity.kt
+++ app/src/main/java/com/qrcode/scanner/MainActivity.kt
@@ -3,18 +3,25 @@ package com.qrcode.scanner
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.qrcode.scanner.data.repository.AppUpdateRepository
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var updateRepository: AppUpdateRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val tv = TextView(this).apply {
-            text = "MainActivity plain"
+            text = "MainActivity + Hilt"
             setTextColor(0xFFFF0000.toInt())
             textSize = 24f
             textAlignment = TextView.TEXT_ALIGNMENT_CENTER
         }
         setContentView(tv)
-        window.decorView.setBackgroundColor(0xFF90EE90.toInt()) // light green
+        window.decorView.setBackgroundColor(0xFFFFE4B5.toInt()) // moccasin
     }
 }
