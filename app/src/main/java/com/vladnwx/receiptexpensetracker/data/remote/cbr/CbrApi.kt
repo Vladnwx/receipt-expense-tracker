5f9a1f11c686d1635288a3661ca8ca5106f42686
anykey196 <anykey196@gmqil.com>
2026-05-21 09:29:14 +0500

    fix: CBR API endpoint daily_json.json → daily_json.js (.json отдаёт 301 → HTML)

--- app/src/main/java/com/vladnwx/receiptexpensetracker/data/remote/cbr/CbrApi.kt
+++ app/src/main/java/com/vladnwx/receiptexpensetracker/data/remote/cbr/CbrApi.kt
@@ -3,6 +3,6 @@ package com.vladnwx.receiptexpensetracker.data.remote.cbr
 import retrofit2.http.GET
 
 interface CbrApi {
-    @GET("daily_json.json")
+    @GET("daily_json.js")
     suspend fun getDailyRates(): CbrDailyResponse
 }
